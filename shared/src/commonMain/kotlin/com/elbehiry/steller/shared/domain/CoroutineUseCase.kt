18054630fcc1d6a3d7c453ed78660f280805c9d5
elbehiry <m.elbehiry44@gmail.com>
2020-10-02 22:46:45 +0200

    impl base useCase uses coroutine and flowUseCase with result object

---
+++ shared/src/commonMain/kotlin/com/elbehiry/steller/shared/domain/CoroutineUseCase.kt
@@ -0,0 +1,35 @@
+package com.elbehiry.steller.shared.domain
+
+import com.elbehiry.steller.shared.result.Result
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+/**
+ * Executes business logic synchronously or asynchronously using Coroutines.
+ */
+abstract class UseCase<in P, R>(private val coroutineDispatcher: CoroutineDispatcher) {
+
+    /** Executes the use case asynchronously and returns a [Result].
+     *
+     * @return a [Result].
+     *
+     * @param parameters the input parameters to run the use case with
+     */
+    suspend fun invoke(parameters: P): Result<R> {
+        return try {
+            withContext(coroutineDispatcher) {
+                execute(parameters).let {
+                    Result.Success(it)
+                }
+            }
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    /**
+     * Override this to set the code to be executed.
+     */
+    @Throws(RuntimeException::class)
+    protected abstract suspend fun execute(parameter: P): R
+}
