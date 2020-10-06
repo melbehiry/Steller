6e9200af4174582f2902f2ef0fd806dd8d90724b
elbehiry <m.elbehiry44@gmail.com>
2020-10-06 22:50:29 +0200

    change gePhotoUseCase to be FlowUseCase

--- shared/src/commonMain/kotlin/com/elbehiry/steller/shared/domain/photos/GetPhotosUseCase.kt
+++ shared/src/commonMain/kotlin/com/elbehiry/steller/shared/domain/photos/GetPhotosUseCase.kt
@@ -1,19 +1,23 @@
 package com.elbehiry.steller.shared.domain.photos
 
 import com.elbehiry.steller.shared.data.photo.PhotoRepository
+import com.elbehiry.steller.shared.domain.FlowUseCase
 import com.elbehiry.steller.shared.domain.UseCase
 import com.elbehiry.steller.shared.model.Photo
+import com.elbehiry.steller.shared.result.Result
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
 import kotlin.jvm.JvmStatic
 
 open class GetPhotosUseCase(
     private val photoRepository: PhotoRepository,
     coroutineDispatcher: CoroutineDispatcher
-) : UseCase<GetPhotosUseCase.Params, List<Photo>>(coroutineDispatcher) {
+) : FlowUseCase<GetPhotosUseCase.Params, List<Photo>>(coroutineDispatcher) {
 
-    override suspend fun execute(parameter: Params): List<Photo> {
-        return photoRepository.getPhotos(parameter.page, parameter.perPage, parameter.orderBy)
+    override suspend fun execute(parameters: Params): Flow<Result<List<Photo>>> {
+        return photoRepository.getPhotos(parameters.page, parameters.perPage, parameters.orderBy)
     }
+
 
     class Params private constructor(val page: Int?, val perPage: Int?, val orderBy: String?) {
         companion object {
