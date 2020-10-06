4d284fae8cfe59ad9bc8ba1b5f352db1d1ad7042
elbehiry <m.elbehiry44@gmail.com>
2020-10-06 22:47:17 +0200

    update dependencies Versions

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,8 @@ object Versions {
     const val kotlinxSerialization = "1.0.0-RC"
     const val koin = "3.0.0-alpha-4"
     const val sqlDelight = "1.4.2"
-    const val gradleVersion = "4.2.0-alpha12"
+    const val gradleVersion = "4.2.0-alpha13"
+    const val androidXcore = "1.5.0-alpha02"
 
     const val sqliteJdbcDriver = "3.30.1"
     const val floorPlan = "0.2"
@@ -17,7 +18,7 @@ object Versions {
     const val room = "2.1.0-beta01"
     const val lifecycle = "2.2.0-alpha01"
     const val fragment = "1.1.0-alpha09"
-    const val compose = "1.0.0-alpha03"
+    const val compose = "1.0.0-alpha04"
     const val coilVersion = "0.2.1"
     const val composeRouterVersion = "0.18.0"
 
@@ -39,6 +40,7 @@ object Compose {
     const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
     const val material = "androidx.compose.material:material:${Versions.compose}"
     const val runtimeLiveData =  "androidx.compose.runtime:runtime-livedata:${Versions.compose}"
+    const val runtime =  "androidx.compose.runtime:runtime:${Versions.compose}"
 }
 
 object Koin {
