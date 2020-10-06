ff3ff8356d6750a596b7d5fae33cf330a6f22c0e
elbehiry <m.elbehiry44@gmail.com>
2020-10-06 22:49:37 +0200

    make photo repo return values as Flow

--- shared/src/commonMain/kotlin/com/elbehiry/steller/shared/data/photo/PhotoRepository.kt
+++ shared/src/commonMain/kotlin/com/elbehiry/steller/shared/data/photo/PhotoRepository.kt
@@ -1,8 +1,10 @@
 package com.elbehiry.steller.shared.data.photo
 
 import com.elbehiry.steller.shared.model.Photo
+import com.elbehiry.steller.shared.result.Result
+import kotlinx.coroutines.flow.Flow
 
 interface PhotoRepository {
 
-    suspend fun getPhotos(page: Int?, perPage: Int?, orderBy: String?): List<Photo>
+    suspend fun getPhotos(page: Int?, perPage: Int?, orderBy: String?): Flow<Result<List<Photo>>>
 }
