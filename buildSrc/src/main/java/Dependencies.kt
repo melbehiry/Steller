e547367a54d81bd01842e54b831f546b2ffcca05
elbehiry <m.elbehiry44@gmail.com>
2021-08-17 20:47:33 +0200

    Update app libraries dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,26 +1,20 @@
 
 object Versions {
-    const val kotlin = "1.4.10"
-    const val kotlinCoroutines = "1.3.9-native-mt"
+    const val kotlin = "1.5.10"
+    const val kotlinCoroutines = "1.5.1"
     const val ktor = "1.4.0"
     const val kotlinxSerialization = "1.0.0-RC"
-    const val koin = "3.0.0-alpha-4"
-    const val sqlDelight = "1.4.2"
-    const val gradleVersion = "4.2.0-alpha13"
+    const val koin = "3.1.2"
+    const val gradleVersion = "7.0.0"
     const val androidXcore = "1.5.0-alpha02"
-
-    const val sqliteJdbcDriver = "3.30.1"
-    const val floorPlan = "0.2"
-    const val slf4j = "1.7.30"
-    const val ktx = "1.0.1"
-    const val nav = "2.1.0-alpha04"
-    const val work = "2.1.0-alpha02"
-    const val room = "2.1.0-beta01"
     const val lifecycle = "2.2.0-alpha01"
-    const val fragment = "1.1.0-alpha09"
-    const val compose = "1.0.0-alpha04"
-    const val coilVersion = "0.2.1"
-    const val composeRouterVersion = "0.18.0"
+    const val compose = "1.0.0"
+    const val composeNavigation = "2.4.0-alpha05"
+    const val composeActivity = "1.3.0"
+    const val composeConstraint = "1.0.0-beta01"
+    const val composePaging = "1.0.0-alpha11"
+    const val composeViewModel = "1.0.0-alpha07"
+    const val accompanist = "0.15.0"
 
     const val junit = "4.13"
     const val testRunner = "1.3.0"
@@ -29,24 +23,37 @@ object Versions {
 
 object AndroidSdk {
     const val min = 21
-    const val compile = 29
+    const val compile = 30
     const val target = compile
 }
 
 object Compose {
-    const val ui = "androidx.compose.ui:ui:${Versions.compose}"
-    const val uiGraphics = "androidx.compose.ui:ui-graphics:${Versions.compose}"
-    const val uiTooling = "androidx.ui:ui-tooling:${Versions.compose}"
-    const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
-    const val material = "androidx.compose.material:material:${Versions.compose}"
-    const val runtimeLiveData =  "androidx.compose.runtime:runtime-livedata:${Versions.compose}"
-    const val runtime =  "androidx.compose.runtime:runtime:${Versions.compose}"
+    const val COMPOSE_UI = "androidx.compose.ui:ui:${Versions.compose}"
+    const val COMPOSE_UI_GRAPHICS = "androidx.compose.ui:ui-graphics:${Versions.compose}"
+    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+    const val COMPOSE_FOUNDATION_LAYOUT = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
+    const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Versions.compose}"
+    const val COMPOSE_RUNTIME_LIVEDATA = "androidx.compose.runtime:runtime-livedata:${Versions.compose}"
+    const val COMPOSE_RUNTIME = "androidx.compose.runtime:runtime:${Versions.compose}"
+    const val COMPOSE_ANIMATION = "androidx.compose.animation:animation:${Versions.compose}"
+    const val COMPOSE_ANIMATION_CORE = "androidx.compose.animation:animation-core:${Versions.compose}"
+    const val COMPOSE_NAVIGATION = "androidx.navigation:navigation-compose:${Versions.composeNavigation}"
+    const val COMPOSE_ACTIVITY = "androidx.activity:activity-compose:${Versions.composeActivity}"
+    const val COMPOSE_CONSTRAINT = "androidx.constraintlayout:constraintlayout-compose:${Versions.composeConstraint}"
+    const val COMPOSE_ICON = "androidx.compose.material:material-icons-extended:${Versions.compose}"
+    const val COMPOSE_TEST = "androidx.compose.ui:ui-test-junit4:1.0.0-rc01:${Versions.compose}"
+    const val COMPOSE_PAGING = "androidx.paging:paging-compose:${Versions.composePaging}"
+    const val COMPOSE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.composeViewModel}"
+    const val INSETS = "com.google.accompanist:accompanist-insets:${Versions.accompanist}"
+    const val COIL = "com.google.accompanist:accompanist-coil:${Versions.accompanist}"
 }
 
 object Koin {
-    val core = "org.koin:koin-core:${Versions.koin}"
-    val android = "org.koin:koin-android:${Versions.koin}"
-    val androidViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    val core = "io.insert-koin:koin-core:${Versions.koin}"
+    val android = "io.insert-koin:koin-android:${Versions.koin}"
+    val jetpackCompose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
+    val Ktor = "io.insert-koin:koin-ktor:${Versions.koin}"
+    val Logger = "io.insert-koin:koin-logger-slf4j:${Versions.koin}"
 }
 
 
